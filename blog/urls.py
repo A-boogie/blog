f72e8b06e85b36dc8febceae35004aa229b5af25
A-boogie <amukelani.s@gmail.com>
2023-07-31 21:18:20 +0200

    DeleteView on Forms

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from .views import (
     BlogDetailView,
     BlogCreateView,
     BlogUpdateView,
+    BlogDeleteView,
 )
 
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
     path("", BlogListView.as_view(), name="home"),
 ]
